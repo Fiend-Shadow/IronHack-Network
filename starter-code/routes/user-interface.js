b9f1be157b71723b76fee2c9054dad6b49a7e585
David Bronne <davidbronne@gmail.com>
2020-02-14 14:15:55 +0100

    User interface route start

---
+++ starter-code/routes/user-interface.js
@@ -0,0 +1,10 @@
+// var express = require('express');
+// var userInterfaceRouter = express.Router();
+
+// const User = require ('./../models/users');
+// const Post = require ('./../models/posts');
+// const Cohort = require ('./../models/cohorts');
+
+// userInterfaceRouter.get('/', (req,res,next) => res.render(''))
+
+// module.export = userInterfaceRouter;

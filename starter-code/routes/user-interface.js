66eb7330e902d85068b45fa62c2f885f6e7ab646
David Bronne <davidbronne@gmail.com>
2020-02-16 13:21:44 +0100

    user-interface  .js  & .hbs initial set up

--- starter-code/routes/user-interface.js
+++ starter-code/routes/user-interface.js
@@ -1,10 +1,40 @@
-// var express = require('express');
-// var userInterfaceRouter = express.Router();
+var express = require('express');
+var userInterfaceRouter = express.Router();
 
-// const User = require ('./../models/users');
-// const Post = require ('./../models/posts');
-// const Cohort = require ('./../models/cohorts');
+const User = require ('./../models/users');
+const Post = require ('./../models/posts');
+const Cohort = require ('./../models/cohorts');
 
-// userInterfaceRouter.get('/', (req,res,next) => res.render(''))
+userInterfaceRouter.use((req,res,next) => {
+    if (req.session.currentUser) {
+        next();
+    } 
+    else  {
+        res.redirect("/login");
+    }
+});
 
-// module.export = userInterfaceRouter;
+userInterfaceRouter
+    .get('/', (req,res,next) => {
+        res.render('user-interface')
+    });
+
+
+
+
+
+// userInterfaceRouter
+//     .post('/', (req, res, next) => {
+
+//     });
+
+userInterfaceRouter.get("/logout", (req, res, next) => {
+    req.session.destroy((err) => {
+        res.redirect("/login");
+    })
+});
+
+
+
+
+module.export = userInterfaceRouter;

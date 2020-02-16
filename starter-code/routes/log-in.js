ad929409b738663354f3d2965c35c70be43a123c
David Bronne <davidbronne@gmail.com>
2020-02-16 13:26:58 +0100

    log-in.js copied for test

--- starter-code/routes/log-in.js
+++ starter-code/routes/log-in.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const logInRouter = express.Router();
 const bcrypt = require("bcrypt");
-
 const User = require("./../models/users");
-
 logInRouter.post("/",(req,res,next) => {
-    const { username , password} = req.body;
-
+    const {username , password} = req.body;
     if (password === "" || username === ""){
         res.render("log-in",{errorMessage:"Username and Password are requiered !"});
         return;
@@ -16,26 +13,20 @@ logInRouter.post("/",(req,res,next) => {
         if (!user){
             res.render("log-in",{errorMessage : "The username doesnt exist!"});
         }
-
         const passwordFromDb = user.password;
         const passwordCorrect = bcrypt.compareSync(password, passwordFromDb);
          if (passwordCorrect){
              req.session.currentUser = user;
-             res.redirect("/");
+             res.redirect("user-interface");
          }
          else {
              res.render("log-in", {errorMessage: "Incorrect password!"});
          }
-
     }).catch((err) => {
         console.log(err);
     });
-    
 })
-
-
 logInRouter.get("/", (req,res,next) => {
     res.render("log-in");
 });
-
 module.exports=logInRouter;
